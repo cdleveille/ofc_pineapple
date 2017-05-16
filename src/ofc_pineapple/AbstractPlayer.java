c0e7bff247e1fa7a979ca96f70d75d811c065304
Chris Leveille <cdleveille@gmail.com>
2017-05-15 21:55:04 -0400

    Second commit

--- src/ofc_pineapple/AbstractPlayer.java
+++ src/ofc_pineapple/AbstractPlayer.java
@@ -1,5 +1,13 @@
 package ofc_pineapple;
 
 public abstract class AbstractPlayer {
-
+	private String name;
+	private CardGroup board[];
+	private CardGroup hand;
+	
+	public AbstractPlayer(String name) {
+		this.name = name;
+		board = new CardGroup[3];
+		hand = new CardGroup();
+	}
 }
